04fb94c0f3ebf09a5566deba7cefc0df1b1a495c
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-03-09 17:33:32 +0900

    Init: 프론트 프로젝트 생성 및 모듈화

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
+
+export default function Home() {
+  const nickname = useSelector((state: RootState) => state.member.nickname);
+  return (
+    <div>
+      <div>홈입니다</div>
+      <div>닉네임: {nickname}</div>
+    </div>
+  );
+}
