e49bb530d740498947d3c293023dff1f67d679e2
ss0takk <ss0takk@gmail.com>
2023-03-13 17:35:47 +0900

    Feat: Response 객체 생성

---
+++ back/sock/src/main/java/com/holo/sock/common/result/Result.java
@@ -0,0 +1,11 @@
+package com.holo.sock.common.result;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Result {
+    private int code;
+    private String message;
+}
