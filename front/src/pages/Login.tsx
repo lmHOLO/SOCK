86bf90a7637e7ceacfac6b82e935189c03b84f1b
chaeeun <codms1104@naver.com>
2023-03-15 00:11:32 +0900

    Design: 화면 margin 값 설정

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LoginBtn from '@/components/Login/LoginBtn';
+// import Nickname from '@/components/Login/Nickname';
 import styles from '@/styles/login.module.css';
 
 export default function Login() {
