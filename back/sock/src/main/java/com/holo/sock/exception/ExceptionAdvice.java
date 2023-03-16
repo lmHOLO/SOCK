d3b370a90c11df04ada85c5c8c0dfedf8ab1cddf
ss0takk <ss0takk@gmail.com>
2023-03-16 23:38:43 +0900

    Feat: Snack 생성

--- back/sock/src/main/java/com/holo/sock/exception/ExceptionAdvice.java
+++ back/sock/src/main/java/com/holo/sock/exception/ExceptionAdvice.java
@@ -1,7 +1,12 @@
 package com.holo.sock.exception;
 
 import com.holo.sock.common.response.ResponseService;
+import com.holo.sock.common.result.Result;
+import com.holo.sock.exception.type.TypeNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -10,4 +15,9 @@ public class ExceptionAdvice {
 
     private final ResponseService responseService;
 
+    @ExceptionHandler(TypeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Result typeNotFoundException(){
+        return responseService.getFailureResult(-100, "해당 과자 종류를 찾을 수 없습니다.");
+    }
 }
