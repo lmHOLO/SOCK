4facff00c7a27e493e8c02e14af97cb5d3827a59
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-03-17 14:09:00 +0900

    Design: Home 추천리스트

--- front/src/components/Home/AdItem.tsx
+++ front/src/components/Home/AdItem.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Paper, Button } from '@mui/material';
-import { AdType } from './Ads';
-type AdProps = {
-  ad: AdType;
-};
+import { Paper } from '@mui/material';
+import { Ad } from '@/components/types';
+interface Props {
+  ad: Ad;
+}
 
-export default function AdItem({ ad }: AdProps) {
+export default function AdItem({ ad }: Props) {
   return (
     <Paper style={{ width: '100%', height: '28vh', margin: '8px 0px' }}>
       <img src={ad.image} alt={ad.title} />
