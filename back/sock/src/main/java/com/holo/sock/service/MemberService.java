4356e2f922e0f3dab2986c170b9e82fb92359dda
geon1120@gmail.com <geon1120@gmail.com>
2023-03-18 22:13:02 +0900

    Feat: append randomString 초기 가입시 닉네임에 무작위 문자열 추가

--- back/sock/src/main/java/com/holo/sock/service/MemberService.java
+++ back/sock/src/main/java/com/holo/sock/service/MemberService.java
@@ -1,10 +1,16 @@
 package com.holo.sock.service;
 
+import com.holo.sock.entity.member.Member;
 import com.holo.sock.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -12,4 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+
+
 }
