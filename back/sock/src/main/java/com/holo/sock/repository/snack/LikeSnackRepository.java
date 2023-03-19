8f076303066f0d073865e740463afd225f2df224
ss0takk <ss0takk@gmail.com>
2023-03-19 16:35:10 +0900

    Feat: 과자 좋아요 취소

--- back/sock/src/main/java/com/holo/sock/repository/snack/LikeSnackRepository.java
+++ back/sock/src/main/java/com/holo/sock/repository/snack/LikeSnackRepository.java
@@ -6,8 +6,11 @@ import com.holo.sock.entity.snack.Snack;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikeSnackRepository extends JpaRepository<LikeSnack, Long> {
 
     boolean existsByMemberAndSnack(Member member, Snack snack);
+    Optional<LikeSnack> findByMemberAndSnack(Member member, Snack snack);
 }
