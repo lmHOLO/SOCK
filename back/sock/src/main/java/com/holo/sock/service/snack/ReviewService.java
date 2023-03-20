3027a1a4d84e5fe54a42f68ddd11db4392103ae7
ss0takk <ss0takk@gmail.com>
2023-03-20 22:47:29 +0900

    Feat: 과자 리뷰 목록 조회

--- back/sock/src/main/java/com/holo/sock/service/snack/ReviewService.java
+++ back/sock/src/main/java/com/holo/sock/service/snack/ReviewService.java
@@ -1,6 +1,7 @@
 package com.holo.sock.service.snack;
 
-import com.holo.sock.dto.snack.request.RegisterReviewRequestDto;
+import com.holo.sock.dto.review.request.RegisterReviewRequestDto;
+import com.holo.sock.dto.review.response.ReviewResponseDto;
 import com.holo.sock.entity.member.Member;
 import com.holo.sock.entity.snack.Review;
 import com.holo.sock.entity.snack.Snack;
@@ -10,8 +11,12 @@ import com.holo.sock.repository.snack.ReviewRepository;
 import com.holo.sock.repository.snack.SnackRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -21,6 +26,15 @@ public class ReviewService {
 
     private final SnackRepository snackRepository;
     private final ReviewRepository reviewRepository;
+
+    public ReviewResponseDto reviewList(Member loginMember, Long snackId, Pageable pageable){
+        Snack snack = snackRepository.findById(snackId).orElseThrow(SnackNotFoundException::new);
+
+        Optional<Review> myReview = reviewRepository.findWithWriter(loginMember, snack);
+        Page<Review> otherReviews = reviewRepository.findReviewListWithWriter(loginMember, snack, pageable);
+
+        return new ReviewResponseDto(myReview, otherReviews);
+    }
 
     @Transactional
     public void registerReview(Long snackId, RegisterReviewRequestDto requestDto, Member writer){
