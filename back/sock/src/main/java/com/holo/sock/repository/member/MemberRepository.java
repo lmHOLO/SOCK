1aa2ff247813cc66c73d5d64e5735695fc5cbd26
geon1120@gmail.com <geon1120@gmail.com>
2023-03-21 15:28:21 +0900

    Refactor: MemberSearch에 Paging추가

--- back/sock/src/main/java/com/holo/sock/repository/member/MemberRepository.java
+++ back/sock/src/main/java/com/holo/sock/repository/member/MemberRepository.java
@@ -2,7 +2,10 @@ package com.holo.sock.repository.member;
 
 import com.holo.sock.common.config.security.oauth2.userinfo.AuthProvider;
 import com.holo.sock.entity.member.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +17,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Long countByNickname(String nickname);
 
-    List<Member> findByNicknameContaining(String nickname);
+    @Query("select m from Member m where m.nickname like concat('%', :nickname, '%') and m.id <> :loginMemberId")
+    Page<Member> findByNicknameContaining(String nickname, Long loginMemberId, Pageable pageable);
 
     Optional<Member> findByEmail(String email);
 
