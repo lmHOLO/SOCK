535a6a2c542bd766baa61e2e99d359dd0006e632
tabss2003 <tabss2003@naver.com>
2023-03-23 15:31:09 +0900

    Refactor: 레시피 exp 추가

--- back/sock/src/main/java/com/holo/sock/repository/recipe/LikeRecipeRepository.java
+++ back/sock/src/main/java/com/holo/sock/repository/recipe/LikeRecipeRepository.java
@@ -3,8 +3,6 @@ package com.holo.sock.repository.recipe;
 import com.holo.sock.entity.member.Member;
 import com.holo.sock.entity.recipe.LikeRecipe;
 import com.holo.sock.entity.recipe.Recipe;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- back/sock/src/main/java/com/holo/sock/service/recipe/RecipeService.java
+++ back/sock/src/main/java/com/holo/sock/service/recipe/RecipeService.java
@@ -22,8 +22,6 @@ import com.holo.sock.repository.snack.SnackRepository;
 import com.holo.sock.service.qscore.RecipeQScoreService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,7 +41,6 @@ public class RecipeService {
     private final LikeRecipeRepository likeRecipeRepository;
     private final RecipeQScoreService recipeQScoreService;
     private final CommentRepository commentRepository;
-
     private final MemberRepository memberRepository;
 
     @Transactional
@@ -129,27 +126,22 @@ public class RecipeService {
 
     @Transactional
     public RecipeDetailResponseDto detailRecipe(Member loginMember, Long recipeId){
-        // 레시피 아이디별로 레시피 찾기
+
         Recipe recipe = recipeRepository.findFetchJoinById(recipeId).orElseThrow(RecipeNotFoundException::new);
 
-        // 해당 레시피의 좋아요 가져오기
         Long totalLikes = likeRecipeRepository.countByRecipe(recipe);
 
-        // loginMember에 따라 해당 레시피의 좋아요를 눌렀는지도 가져오기
         boolean existsLike = likeRecipeRepository.existsByMemberAndRecipe(loginMember, recipe);
 
-        // 해당 레시피의 이미지 가져오기
         List<RecipeImageDto> recipeImageDtoList = recipe.getImages().stream()
                 .map(recipeImage -> new RecipeImageDto(recipeImage))
                 .collect(Collectors.toList());
 
-        // 레시피에 해당한 태그 가져오기
         List<Tag> tagList = tagRepository.findAllFetchJoinByRecipe(recipe);
         List<TagDto> tagDtoList = tagList.stream()
                 .map(tag -> new TagDto(tag))
                 .collect(Collectors.toList());
 
-        // 레시피 인기도 증가
         recipeQScoreService.addQScore(recipe);
 
         return new RecipeDetailResponseDto(recipe,tagDtoList,recipeImageDtoList,existsLike,totalLikes);
@@ -157,15 +149,10 @@ public class RecipeService {
     }
     @Transactional
     public void updateRecipeDetail(Member loginMember,Long recipeId, UpdateRecipeRequestDto updateDto){
-        // 해당 번호 레시피 아이디 찾기 , 해당 회원
         Recipe recipe = recipeRepository.findByWriterAndId(loginMember,recipeId).orElseThrow(RecipeNotFoundException::new);
 
         List<Tag> tags = tagRepository.findAllByRecipe(recipe);
-        //업데이트 - 변경감지
-        // recipe.updateDetail , 제목 내용 태그 이미지만 바꿀 수 있다
 
-        //이미지를 전부 삭제했다가 다시 builder
-        // 등록된 레시피에 있는 첫번째 이미지 제거 -> 등록되어 있던 image과의 관계 끊어짐
         recipe.getImages().clear();
 
         List<String> imageName = updateDto.getImages();
@@ -180,8 +167,6 @@ public class RecipeService {
 
         recipe.updateRecipe(updateDto.getTitle(), updateDto.getContent(), recipe.getImages());
 
-
-        // 1. 태그를 전부 삭제하고 다시 builder
         tagRepository.deleteAllInBatch(tags);
 
         List<Long> snackIds = updateDto.getSnackIds();
@@ -199,7 +184,6 @@ public class RecipeService {
     }
 
     public List<LikeRecipeResponseDto> likeRecipeList(Long memberId){
-        // 해당 회원이 좋아요 누른 레시피
         Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
 
         return member.getLikeRecipes().stream()
