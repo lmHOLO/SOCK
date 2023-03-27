331c98e5432ef81d8a1cffc0289df2a9c2e68b92
민우 <meanwo0603@naver.com>
2023-03-27 16:27:47 +0900

    Feat: 협업 필터링을 활용한 추천 리스트

--- AI/fastapi_mysql/snack_db.py
+++ AI/fastapi_mysql/snack_db.py
@@ -6,10 +6,13 @@
 from typing import Optional, List
 import pandas as pd
 import numpy as np
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sqlalchemy import create_engine
 from random import randint
+from surprise import SVD, Reader
+from surprise.dataset import DatasetAutoFolds
 
 class Item(BaseModel):
     member_id: int
@@ -43,6 +46,8 @@ def sigmoid_transform_search(x):
     # 결과값을 0~5 사이의 값으로 변환
     y = 5 * y
     return y
+
+
 
 app = FastAPI()
 
@@ -123,7 +128,7 @@ def recommend():
     cursor = connection.cursor()
     # purchase db에서 member_id, snack_id, count 가져오기
     # review db에서 writer_id, snack_id, star 가져오기
-    # search db에서 member_id, snack_id, count 가져오가
+    # search db에서 member_id, snack_id, count 가져오기
     purchase_sql = "SELECT member_id, snack_id, count FROM sock.purchase;"
     cursor.execute(purchase_sql)
     purchase_cursor = cursor.fetchall()
@@ -151,25 +156,28 @@ def recommend():
     member_snack_arr = [[0]*729 for _ in range(9)]
 
     # 이것을 나눌 분모 테이블
-    demoni = [[0]*729 for _ in range(9)]
+    demoni = [[6]*729 for _ in range(9)]
+
 
     # 검색에 관련된 평점
     for i in range(len(search_df)):
         member_snack_arr[search_df.loc[i, 'member_id']][search_df.loc[i, 'snack_id']] += search_df.loc[i, 'count']
-        demoni[search_df.loc[i, 'member_id']][search_df.loc[i, 'snack_id']] += 1
+        # demoni[search_df.loc[i, 'member_id']][search_df.loc[i, 'snack_id']] += 1
     # print(member_snack_arr)
     
-    
+    # 검색에 참여한 유저의 숫자 구하기 (테스트 데이터 : user_num = 8)
+    user_num = search_df.iloc[-1]['member_id'] 
+
     # 구매에 관련된 평점
     for i in range(len(purchase_df)):
         member_snack_arr[purchase_df.loc[i, 'member_id']][purchase_df.loc[i, 'snack_id']] += purchase_df.loc[i, 'count']*2
-        demoni[purchase_df.loc[i, 'member_id']][purchase_df.loc[i, 'snack_id']] += 2
+        # demoni[purchase_df.loc[i, 'member_id']][purchase_df.loc[i, 'snack_id']] += 2
     # print(member_snack_arr)
 
     # 리뷰에 관련된 평점
     for i in range(len(review_df)):
         member_snack_arr[review_df.loc[i, 'writer_id']][review_df.loc[i, 'snack_id']] += review_df.loc[i, 'star']*3
-        demoni[review_df.loc[i, 'writer_id']][review_df.loc[i, 'snack_id']] += 3
+        # demoni[review_df.loc[i, 'writer_id']][review_df.loc[i, 'snack_id']] += 3
     # print(member_snack_arr)
     # print(demoni)
 
@@ -186,11 +194,67 @@ def recommend():
         'snack_id': non_zero_indices[1],
         'rating': np.around(result[non_zero_indices], decimals = 2)
     })
+    
+    total_rating.to_csv('total_rating_noh.csv', index=False, header=False)
 
-    # total_rating_json = total_rating.to_json('/total_rating_json.json', orient="records")
     total_rating_json = total_rating.to_json(orient="records")
 
-    # purchase_json = purchase_df.to_json(orient="records")
-    # search_json = search_df.to_json(orient="records")
-    return total_rating_json
+    # surprise 를 활용한 협업 필터링 구현 부분
+    reader = Reader(line_format='user item rating', sep=',', rating_scale=(0, 5))
+    data_folds = DatasetAutoFolds('total_rating_noh.csv', reader=reader)
+    trainset = data_folds.build_full_trainset()
+
+    model = SVD(n_epochs=20, n_factors=50, random_state=0)
+    model.fit(trainset)
+    snack_db = pd.read_csv('snack_db_test.csv', encoding='cp949')
+    def get_unseen_surprise(ratings, snack_db, member_id):
+    #입력값으로 들어온 userId에 해당하는 사용자가 평점을 매긴 모든 과자를 리스트로 생성
+        seen_snacks = ratings[ratings['member_id']== member_id]['snack_id'].tolist()
+
+        #모든 과자들의 snack_id를 리스트로 생성.
+        total_snacks = snack_db['snack_id'].tolist()
+        # print(total_snacks, 'total_snacks')
+        # 모든 과자들의 snack_id 중 이미 평점을 매긴 과자의 snack_id를 제외하여 리스트로 생성
+        unseen_snacks = [snack for snack in total_snacks if snack not in seen_snacks]
+        # print(unseen_snacks)
+        print('평점 매긴 과자수:', len(seen_snacks), '추천대상 과자수:', len(unseen_snacks), '전체 과자:', len(total_snacks))
+        return unseen_snacks
+
+    def recomm_snack_by_surprise(model, member_id, unseen_snacks, top_n=10):
+        predictions = [model.predict(str(member_id), str(snack_id)) for snack_id in unseen_snacks]
+        # print(predictions, 'test')
+        def sortkey_est(pred):
+            return pred.est
+
+        predictions.sort(key=sortkey_est, reverse=True)
+        top_predictions = predictions[:top_n]
+
+        # top_snack_ids = [int(pred.iid) for pred in top_predictions]
+        # top_snack_rating = [pred.est for pred in top_predictions]
+        # top_snack_titles = snack_db[snack_db.snack_id.isin(top_snack_ids)]['이름']
+        
+        top_snack_preds = []
+        for pred in top_predictions:
+            
+            snack_id = int(pred.iid)
+            snack_title = snack_db[snack_db["snack_id"] == snack_id]["이름"].tolist()
+            snack_rating = pred.est
+            # print(f"{snack_id}, {snack_title}: {snack_rating:.2f}")
+            top_snack_preds.append([f"{snack_id}, {snack_title}: {snack_rating:.2f}"])
+        # top_snack_preds = [ (id, title, rating) for id, title, rating in zip(top_snack_ids, top_snack_titles, top_snack_rating)]
+        return top_snack_preds
+    
+    top_snack_preds_list = []
+    for i in range(1, int(user_num)+1):
+        unseen_snacks = get_unseen_surprise(total_rating, snack_db, i+1)
+        top_snack_preds = recomm_snack_by_surprise(model, i, unseen_snacks, top_n=10)
+        top_snack_preds_list.append(top_snack_preds)
+        
+    # unseen_snacks = get_unseen_surprise(total_rating, snack_db, 1)
+    # top_snack_preds = recomm_snack_by_surprise(model, 1, unseen_snacks, top_n=10)
+        top_snack_preds_list_json = json.dumps(top_snack_preds_list, ensure_ascii=False)
+    # top_snack_preds_json = top_snack_preds.to_json(force_ascii=False, orient = "split")
+    
+    return top_snack_preds_list_json
+    # return total_rating_json
     
