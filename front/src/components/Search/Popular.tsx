f5231dbdc1f556c627c8860a989d3edce3fcdab5
ss0takk <ss0takk@gmail.com>
2023-03-29 14:09:34 +0900

    Feat: 인기검색어 출력 페이지

--- front/src/components/Search/Popular.tsx
+++ front/src/components/Search/Popular.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '@/styles/search.module.css';
 import { useNavigate } from 'react-router-dom';
-interface Props {
-  popularList: { snackId: string; snackTitle: string }[];
-}
-export default function Popular({ popularList }: Props) {
+import { PopularType } from '@/types/search';
+
+export default function Popular() {
   const navigate = useNavigate();
+
+  const [popularList, setPopularList] = useState<PopularType[]>([]);
+
   return (
     <div className={styles['popular-container']}>
       <ol className={styles['popular-list']}>
--- front/src/types/search.ts
+++ front/src/types/search.ts
@@ -1,1 +1,7 @@
 export type SearchThemeType = 'snack' | 'recipe' | 'member';
+
+export type PopularType = {
+  id: string;
+  snackCheck: boolean;
+  score: number;
+};
