af27be01f51fc38ebcb81a533ca9e3d0949a4fb8
chaeeun <codms1104@naver.com>
2023-03-29 23:43:17 +0900

    Fix: 로그인 오류

--- front/src/apis/axiosConfig.ts
+++ front/src/apis/axiosConfig.ts
@@ -10,7 +10,7 @@ export function apiInstance(): AxiosInstance {
 }
 export function authApiInstance(): AxiosInstance {
   const token = window.localStorage.getItem('token');
-  // console.log(token);
+  console.log(token);
   const instance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
