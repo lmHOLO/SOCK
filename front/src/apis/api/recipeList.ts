a9c89edd7d1dd46f93cf356695dbc114310d20e4
tabss2003 <tabss2003@naver.com>
2023-03-31 16:59:54 +0900

    Feat: 마이프로필 - 내가 작성한 레시피 목록 수정필요

--- front/src/apis/api/recipeList.ts
+++ front/src/apis/api/recipeList.ts
@@ -1,19 +1,26 @@
 import { authApiInstance } from '@/apis/axiosConfig';
 import { isAxiosError } from 'axios';
 
+export const getRecipeListAPI = async (keyword: string, arrange: string, memberId: string) => {
+  try {
+    let url = `/recipes`;
 
-export const getRecipeListAPI =async (keyword:string, arrange:string,memberId:string) => {
-    try{
-        let url = `/recipes?arrange=${arrange}`;
-        if(`${keyword}`) url = url+`&keyword=${keyword}`;
-        if(`${memberId}`) url = url+`&member-id=${memberId}`;
-        
-        const { data } = await authApiInstance().get(url);
-        return data.data;
-    }catch(error){
-        if (isAxiosError(error)) {
-            console.log('에러: ', error.response);
-            return error.response?.data;
-        }
+    if (`${arrange}`) url = url + `?arrange=${arrange}`;
+
+    if (`${keyword}`) {
+      if (`${arrange}`) url = url + `&keyword=${keyword}`;
+      else url = url + `?keyword=${keyword}`;
     }
-}
+    if (`${memberId}`) {
+      url = url + `?member-id=${memberId}`;
+    }
+
+    const { data } = await authApiInstance().get(url);
+    return data.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.log('에러: ', error.response);
+      return error.response?.data;
+    }
+  }
+};
