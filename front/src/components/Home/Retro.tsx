34fd1d5f2e6d0e1d216dd744d44ede7541b0b3a2
ss0takk <ss0takk@gmail.com>
2023-04-01 20:41:37 +0900

    Feat: 테마별 과자 목록(영화, 추억)

--- front/src/components/Home/Retro.tsx
+++ front/src/components/Home/Retro.tsx
@@ -9,15 +9,19 @@ import { useNavigate } from 'react-router';
 interface Props {
   recommendSnackList: SnackListItemType[];
 }
-export default function Retro({ recommendSnackList }: Props) {
+export default function Retro() {
   const navigate = useNavigate();
+  const clickEvent = () => {
+    navigate('/snack-content/RETRO');
+  };
+
   return (
     <div className={`${styles['theme-container']} ${styles['green']}`}>
       <div className={styles['retro-main']}>
         <img src={require(`@/assets/home/retro_green_main.png`)} alt='이미지' />
       </div>
       <div className={styles['retro-lego']}>
-        <img src={require(`@/assets/home/retro_green_lego.png`)} alt='이미지' />
+        <img src={require(`@/assets/home/retro_green_lego.png`)} alt='이미지' onClick={clickEvent} />
       </div>
     </div>
   );
