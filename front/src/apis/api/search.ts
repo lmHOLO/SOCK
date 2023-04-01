491ca034cf85ed8be8830448a4c33d3d8bd5e5f8
chaeeun <codms1104@naver.com>
2023-04-01 18:37:18 +0900

    Feat: 검색기능

--- front/src/apis/api/search.ts
+++ front/src/apis/api/search.ts
@@ -14,3 +14,27 @@ export const getTopPopularListAPI = async () => {
     }
   }
 };
+
+export const getRecipeListAPI = async (keyword: string, arrange: string, memberId: string) => {
+  try {
+    let url = `/recipes`;
+
+    if (`${arrange}`) url = url + `?arrange=${arrange}`;
+
+    if (`${keyword}`) {
+      if (`${arrange}`) url = url + `&keyword=${keyword}`;
+      else url = url + `?keyword=${keyword}`;
+    }
+    if (`${memberId}`) {
+      url = url + `?member-id=${memberId}`;
+    }
+
+    const { data } = await authApiInstance().get(url);
+    return data.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.log('에러: ', error.response);
+      return error.response?.data;
+    }
+  }
+};
--- front/src/pages/Search.tsx
+++ front/src/pages/Search.tsx
@@ -10,6 +10,12 @@ import { getTopPopularListAPI } from '@/apis/api/search';
 import FilterModal from '@/components/Search/FilterModal';
 import BottomNav from '@/components/Navbar/BottomNav';
 import { FilterType } from '@/types/snack';
+import SnackGridList from '@/components/common/SnackGridList';
+import RecipeGridList from '@/components/common/RecipeGridList';
+import { SnackDetailType } from '@/types/snack';
+import { GridRecipeListItemType } from '@/types/recipe';
+import { getRecipeListAPI } from '@/apis/api/recipeList';
+import { getSnackListAPI } from '@/apis/api/snackList';
 
 export default function Search() {
   const navigate = useNavigate();
@@ -19,12 +25,16 @@ export default function Search() {
     [],
   );
   const [modalOpen, setModalOpen] = useState<boolean>(false);
-
-  // const { keyword, arrange, memberId } = useParams();
+  const [searchClicked, setSearchClicked] = useState<boolean>(false);
   const [filter, setFilter] = useState<FilterType>({
     flavors: [],
     types: [],
   });
+
+  const [sort, setSort] = useState<string>('latest');
+  const [snackList, setSnackList] = useState<SnackDetailType[]>([]);
+  const [recipeList, setRecipeList] = useState<GridRecipeListItemType[]>([]);
+
   const applyFilter = async (newFilter: FilterType) => {
     setFilter(newFilter);
     setModalOpen(false);
@@ -35,12 +45,57 @@ export default function Search() {
     });
   }, []);
 
-  const searchEvent = () => {
+  const handleSort = async (sort: string) => {
+    setSort(sort);
+    if (theme === 'recipe') {
+      getRecipeListAPI(searchBar, sort, '').then((data) => {
+        setRecipeList(data.content);
+      });
+      return;
+    }
     if (theme === 'snack') {
-      navigate(`/snacks`);
+      getSnackListAPI(searchBar, filter.flavors, filter.types, sort).then((data) => {
+        setSnackList(data.content);
+      });
+    }
+  };
+
+  const handleSetTheme = (menu: string) => {
+    switch (menu) {
+      case 'snack':
+        setTheme('snack');
+        break;
+      case 'recipe':
+        setTheme('recipe');
+        break;
+      case 'member':
+        setTheme('member');
+    }
+    setSearchClicked(false);
+    setFilter({
+      flavors: [],
+      types: [],
+    });
+  };
+
+  const handleSearchBtnClick = () => {
+    if (theme === 'snack') {
+      getSnackListAPI(searchBar, filter.flavors, filter.types, sort).then((data) => {
+        setSnackList(data.content);
+      });
+      console.log('snack: ', filter);
+      console.log('searchbar: ', searchBar);
+      setSearchClicked(true);
     } else if (theme === 'recipe') {
-      navigate(`/recipes`, { state: searchBar });
+      console.log('recipe');
+      console.log('searchbar: ', searchBar);
+      getRecipeListAPI('', sort, '').then((data) => {
+        setRecipeList(data.content);
+      });
+      setSearchClicked(true);
     } else if (theme === 'member') {
+      console.log('member');
+      console.log('searchbar: ', searchBar);
     }
   };
 
@@ -52,17 +107,58 @@ export default function Search() {
     <div className='side-margin'>
       <TopNavOnlyBack />
       <div className={styles['search-bar-conatiner']}>
-        <PositionedMenu theme={theme} setTheme={setTheme} />
+        <PositionedMenu theme={theme} setTheme={setTheme} handleSetTheme={handleSetTheme} />
         <input className={styles['search-bar']} type='text' onChange={(e) => handleSearchBar(e)} />
-        <button className={styles['search-btn']} onClick={searchEvent}>
+        <button className={styles['search-btn']} onClick={handleSearchBtnClick}>
           <p>검색</p>
         </button>
       </div>
-      <div className={styles['filter-container']} onClick={() => setModalOpen(true)}>
-        <FilterAltIcon className={styles['filter-btn']} />
+      <div className={styles['filter-bar']}>
+        {searchClicked && (
+          <div className={styles['sort']}>
+            {sort === 'popular' && (
+              <>
+                <button className={styles['bold']}>인기순</button>
+                <button onClick={() => handleSort('latest')}>최신순</button>
+              </>
+            )}
+            {sort === 'latest' && (
+              <>
+                <button onClick={() => handleSort('popular')}>인기순</button>
+                <button className={styles['bold']}>최신순</button>
+              </>
+            )}
+          </div>
+        )}
+        <div className={styles['filter-container']} onClick={() => setModalOpen(true)}>
+          {theme === 'snack' && <FilterAltIcon className={styles['filter-btn']} />}
+        </div>
+      </div>
+      <div>
+        {filter.flavors.map((item, index) => {
+          return (
+            <span key={index} className={`${styles['selected']}`}>
+              {item}
+            </span>
+          );
+        })}
+        {filter.types.map((item, index) => {
+          return (
+            <span key={index} className={`${styles['selected']}`}>
+              {item}
+            </span>
+          );
+        })}
       </div>
       <FilterModal modalOpen={modalOpen} setModalOpen={setModalOpen} filter={filter} applyFilter={applyFilter} />
-      <Popular popularList={popularList} />
+      {!searchClicked && <Popular popularList={popularList} />}
+      {searchClicked &&
+        theme === 'snack' &&
+        (snackList.length ? <SnackGridList snackList={snackList} /> : <p>과자가 없습니다ㅠ</p>)}
+      {searchClicked &&
+        theme === 'recipe' &&
+        (recipeList.length ? <RecipeGridList recipeList={recipeList} /> : <p>레시피가 없습니다ㅠ</p>)}
+
       <BottomNav />
     </div>
   );
