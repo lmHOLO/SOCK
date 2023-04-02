077c9fa5f2d1c9d93348db94bc7e53e7f11c329b
geon1120@gmail.com <geon1120@gmail.com>
2023-04-03 01:29:24 +0900

    Feat: icon image 경로추가

--- front/src/components/Search/FilterModal.tsx
+++ front/src/components/Search/FilterModal.tsx
@@ -54,66 +54,82 @@ export default function FilterModal({ modalOpen, setModalOpen, filter, applyFilt
     {
       id: '1',
       name: '쿠키',
+      image: '@/assets/search/cookie.png',
     },
     {
       id: '2',
       name: '막대과자',
+      image: '@/assets/search/stick.png',
     },
     {
       id: '3',
       name: '감자칩',
+      image: '@/assets/search/potato.png',
     },
     {
       id: '4',
       name: '크래커',
+      image: '@/assets/search/cracker.png',
     },
     {
       id: '5',
       name: '비스킷',
+      image: '@/assets/search/biscuit.png',
     },
     {
       id: '6',
       name: '뻥튀기/건빵',
+      image: '@/assets/search/geonbbang.png',
     },
     {
       id: '7',
       name: '샌드',
+      image: '@/assets/search/sand.png',
     },
     {
       id: '8',
       name: '나쵸',
+      image: '@/assets/search/nacho.png',
     },
     {
       id: '9',
       name: '고구마',
+      image: '@/assets/search/sweet-patato.png',
     },
     {
       id: '10',
       name: '팝콘',
+      image: '@/assets/search/popcorn.png',
     },
     {
       id: '11',
       name: '프레첼',
+      image: '@/assets/search/fretchel.png',
     },
     {
       id: '12',
       name: '옥수수',
+      image: '@/assets/search/corn.png',
     },
     {
       id: '13',
       name: '새우/오징어',
+      image: '@/assets/search/shrimp.png',
     },
     {
       id: '14',
       name: '양파/마늘맛',
+      image: '@/assets/search/onion.png',
     },
     {
       id: '15',
       name: '젤리',
+      image: '@/assets/search/jelly.png',
     },
     {
       id: '16',
       name: '초콜릿',
+      image: '@/assets/search/chocolate.png',
     },
   ];
 
@@ -121,22 +137,27 @@ export default function FilterModal({ modalOpen, setModalOpen, filter, applyFilt
     {
       id: '1',
       name: '달콤한맛',
+      image: '@/assets/search/sweet.png',
     },
     {
       id: '2',
       name: '고소한맛',
+      image: '@/assets/search/nutty.png',
     },
     {
       id: '3',
       name: '새콤한맛',
+      image: '@/assets/search/sour.png',
     },
     {
       id: '4',
       name: '짭짤한맛',
+      image: '@/assets/search/salty.png',
     },
     {
       id: '5',
       name: '매콤한맛',
+      image: '@/assets/search/spicy.png',
     },
   ];
 
