e374b444a9d2789327eca0ceb49820db31df48e8
ss0takk <ss0takk@gmail.com>
2023-04-04 21:56:50 +0900

    Refactor: 테스트 구매 수 데이터 추가

--- back/sock/src/main/java/com/holo/sock/service/data/DataService.java
+++ back/sock/src/main/java/com/holo/sock/service/data/DataService.java
@@ -12,6 +12,7 @@ import com.holo.sock.repository.jdbc.JdbcDataRepository;
 import com.holo.sock.repository.member.MemberRepository;
 import com.holo.sock.repository.snack.SnackRepository;
 import com.holo.sock.service.snack.ReviewService;
+import com.holo.sock.service.snack.SnackService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -27,10 +28,23 @@ public class DataService {
 
     private final JdbcDataRepository jdbcDataRepository;
     private final MemberRepository memberRepository;
+    private final SnackService snackService;
     private final ReviewService reviewService;
 
     public void registerPurchase(List<PurchaseDumpDto> request){
-        jdbcDataRepository.savePurchase(request);
+//        jdbcDataRepository.savePurchase(request);
+        for (PurchaseDumpDto purchaseDumpDto : request) {
+            Long memberId = purchaseDumpDto.getMember_id();
+            Long snackId = purchaseDumpDto.getSnack_id();
+            int count = purchaseDumpDto.getCount();
+
+            Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+
+            for(int i = 0; i < count; i++){
+                snackService.purchaseSnack(member, snackId);
+            }
+        }
+
     }
 
     public void registerReview(List<ReviewDumpDto> request){
